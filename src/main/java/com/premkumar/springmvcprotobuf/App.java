635c47849667288dfd411a60a8813d22c8f255a7
Prem Kumar <premkumar1989@gmail.com>
2017-06-07 20:10:57 +0530

    mvn project

---
+++ src/main/java/com/premkumar/springmvcprotobuf/App.java
@@ -0,0 +1,13 @@
+package com.premkumar.springmvcprotobuf;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
